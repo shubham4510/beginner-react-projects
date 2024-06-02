b6cf03f82708f6ae6183573a399125a16db07a9f
Shubham <shubhambhardwaj112277@gmail.com>
2024-06-02 17:23:18 +0530

    Added Services Page to Project 1

--- one/src/App.jsx
+++ one/src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import Home from "./components/Home.jsx"
 import Footer from './components/Footer.jsx'
 import Contact from "./components/Contact.jsx"
+import Services from "./components/Services.jsx"
 
 
 import './styles/App.scss'
@@ -19,7 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/contact" element={<Contact/>}/>
-        <Route path="/services" element={<Contact/>}/>
+        <Route path="/services" element={<Services/>}/>
       </Routes>
       <Footer/>
     </div>
