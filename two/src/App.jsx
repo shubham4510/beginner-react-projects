c9e186a5fea4d0512ed087db3fd6fb0d455ee587
Shubham <shubhambhardwaj112277@gmail.com>
2024-06-02 21:48:49 +0530

    Created Side Bar for Video Hub

---
+++ two/src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes></Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ two/src/ColorModeSwitcher.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import {FaMoon,FaSun} from 'react-icons/fa'
+
+
+const ColorModeSwitcher = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const icon = colorMode === 'light' ? <FaMoon /> : <FaSun />;
+  const text = useColorModeValue('dark','light');
+const SwitchIcon = useColorModeValue(FaMoon,FaSun)
+
+  return (
+    <IconButton
+      aria-label={`Switch to ${colorMode === 'light' ? 'dark' : 'light'} mode`}
+      onClick={toggleColorMode}
+      pos={'fixed'}
+      top={'4'}
+      right={'4'}
+      icon={icon}
+      {...props}
+    />
+  );
+};
+
+export default ColorModeSwitcher;
